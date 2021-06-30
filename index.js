62fd811c6df1d59cc4607d44642ca51e83bddbcd
Sesugh Asa <itsmason58@gmail.com>
2021-06-30 23:37:31 +0100

    index

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(expressLayoutes);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 app.get('/', function(req, res){
-  res.redirect('/addCustomer');
+  res.redirect('index');
 });
 app.use(express.urlencoded({extended:true}));
 app.use(cors());
