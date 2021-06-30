f39406d2c5dad4dcd8f03f3c654ed794a9e22320
Sesugh Asa <itsmason58@gmail.com>
2021-06-30 23:53:18 +0100

    update

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const path = require('path')
-const cors = require('cors')
 const expressLayoutes = require('express-ejs-layouts');
 const bodyParser = require('body-parser');
 
@@ -15,21 +14,19 @@ require('./startup/config')();
 require('./startup/db')();
 //require('./startup/logging')();
 require('./startup/validations')();
-
-app.use(expressLayoutes);
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
-app.get('/', function(req, res){
-  res.redirect('index');
-});
-app.use(express.urlencoded({extended:true}));
-app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(customerRoutes.routes);
   app.use(expressLayoutes);
   app.use(customerRoutes.routes);
-  app.use(cors())
+  
+app.use(expressLayoutes);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+app.get('/', function(req, res){
+  res.redirect('index');
+});
+//app.use(express.urlencoded({extended:true}));
 
   app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
