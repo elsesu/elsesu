f53cb79544c120f3831deb1c2bca1ba34021f83d
Sesugh Asa <itsmason58@gmail.com>
2021-06-30 22:57:31 +0100

    entire

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const customerRoutes = require('./routes/customer-routes');
 const PORT = process.env.PORT || 5000
 
 app = express()
-require('Procfile')
+require('Procfile');
 require('./startup/config')();
 require('./startup/db')();
 //require('./startup/logging')();
@@ -18,11 +18,12 @@ require('./startup/validations')();
 
 app.use(expressLayoutes);
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({extended:true}));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', (req, res) => res.render('./views/addCustomer'))
+
 app.use(customerRoutes.routes);
   app.use(expressLayoutes);
   app.use(customerRoutes.routes);
