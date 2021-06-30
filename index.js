fe602e0de57f98ebbc06608f0ae5cc4438f7808f
Sesugh Asa <itsmason58@gmail.com>
2021-06-30 23:10:07 +0100

    update index

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ require('./startup/validations')();
 app.use(expressLayoutes);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
+app.get('/', (req, res) => res.render('addCustomer'))
 app.use(express.urlencoded({extended:true}));
 app.use(cors());
 app.use(bodyParser.json());
